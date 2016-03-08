f5a5c3e7de83024d661b59043f9b0f8e5038ee27
andreij6 <andreij6@gmail.com>
2016-03-07 23:10:23 -0500

    Initial Checkin
    Start of the ArcGIS Javascript Application
    
    init

---
+++ Mapp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mapp.MvcApplication" Language="C#" %>
---
+++ Mapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mapp.Startup))]
+namespace Mapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
